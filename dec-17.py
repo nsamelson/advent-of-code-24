53822d7c2b5c6b9471428906c3328b8db7447f8b
Nicolas Samelson <salocin.sam@gmail.com>
2024-12-26 15:47:07 +0100

    uoptimised

--- dec-17.py
+++ dec-17.py
@@ -1,3 +1,4 @@
+import heapq
 import math
 import numpy as np
 from tqdm import tqdm
@@ -49,7 +50,7 @@ def load_data(file_name):
 
 # what is it trying to output
 
-def assembly_instructions(registers, program):
+def assembly_instructions(registers, program, verbose=False):
     out = []
 
     i = 0
@@ -61,7 +62,10 @@ def assembly_instructions(registers, program):
         
         
         # get combo operand
-        combo = operand if operand <=3 else registers[list(registers.keys())[operand - 4]]
+        if operand == 7:
+            combo = None
+        else:
+            combo = operand if operand <=3 else registers[list(registers.keys())[operand - 4]]
 
         match opcode:
         # operate the opcode
@@ -83,13 +87,11 @@ def assembly_instructions(registers, program):
                 registers["B"] = int(registers["A"] / (2**combo))
             case 7:
                 registers["C"] = int(registers["A"] / (2**combo))
-        print("index:",i, "code and op:", opcode, operand, "out:",out, "A:", registers["A"])
-        i +=2
+        if verbose:
+            print("index:",i, "code and op:", opcode, operand, "out:",out, "A:", registers["A"])
+        i +=2    
 
-    out_txt = ",".join(str(num) for num in out)
-    
-
-    return out_txt
+    return out
 
 # Task 2:
 #   - program supposed to output another copy of the program
@@ -97,32 +99,22 @@ def assembly_instructions(registers, program):
 #   - Need to find new A value so the program copies itself
 #   - this means Program input == Program output
 
-def assembly_auto_program(registers, program):
-    # goal = []
-    output = program.copy()
-    registers["A"] = 0 # setup corruped to 0
-
-    # go in reverse and wait till
-    i = len(program) - 2
+def assembly_auto_program(start_registers, program):
+    start_registers["A"] = 0 # setup corruped to 0
     quotients = [0,1,2,3,4,5,6,7]
 
-
-    queue = [(len(program) - 2, program.copy(), registers.copy())] # i, output, registers
+    queue = [(len(program) - 2, program.copy(), start_registers.copy())] # i, output, registers
     
-    while len(output)!= 0 or i !=0:
+    # get the jumper indices
+    jumpers = [j for j,val in enumerate(program) if val == 3 and j % 2 == 0]
 
-        
+    while queue:
+        i, output, registers = queue.pop(0)
 
         opcode = program[i]
         operand = program[i+1]
 
-        
-
-        # get the jumper indices
-        jumpers = [j for j,val in enumerate(program) if val == 3 and j % 2 == 0]
         jump_index = [val for val in jumpers if program[val+1] == i]
-        
-             
         
         # get combo operand
         if operand == 7:
@@ -130,40 +122,54 @@ def assembly_auto_program(registers, program):
         else:
             combo = operand if operand <=3 else registers[list(registers.keys())[operand - 4]]
 
-        match opcode:
+        iters = [0] if opcode in [1,2,3,4,5] else quotients
+
         # operate the opcode
-            case 0:
-                registers["A"] = registers["A"] * (2**combo)
-            case 1:
-                registers["B"] = registers["B"] ^ operand
-            case 2:
-                registers["B"] = combo * 8 + registers["B"]
-            case 3:
-                pass
-            case 4:
-                registers["B"] = registers["B"] ^ registers["C"] 
-            case 5:
-                res = combo * 8 + output.pop()
-                if 3 < operand <7:
-                    registers[list(registers.keys())[operand - 4]] = res
-            case 6:
-                registers["A"] = registers["B"] * (2**combo)
-            case 7:
-                registers["A"] = registers["C"] * (2**combo)
-        
-        
-        print("index:",i, "code and op:", opcode, operand, "out:",output, "A:", registers["A"])
+        for j in iters:
+            new_reg = registers.copy()
+            new_output = output[:]
+            match opcode:
+                case 0:
+                    new_reg["A"] = new_reg["A"] * (2**combo) + j
+                case 1:
+                    new_reg["B"] = new_reg["B"] ^ operand
+                case 2:
+                    new_reg["B"] = combo * 8 + new_reg["B"]
+                case 3:
+                    pass
+                case 4:
+                    new_reg["B"] = new_reg["B"] ^ new_reg["C"] 
+                case 5:
+                    if len(new_output) ==0:
+                        continue
+                    popped = new_output.pop()
+                    res = combo * 8 + popped
+                    # print(new_reg[list(new_reg.keys())[operand - 4]], res, popped)
+                    # if 3 < operand <7:
+                    #     new_reg[list(new_reg.keys())[operand - 4]] = res
+                case 6:
+                    new_reg["A"] = new_reg["B"] * (2**combo) + j
+                case 7:
+                    new_reg["A"] = new_reg["C"] * (2**combo) + j
 
-        # jumps if one correct index and A != 0
-        if len(jump_index) !=0 : #and registers["A"] != 0:
-            # print("Jumping from",i, "to", jump_index)
-            i = jump_index[0]
-            continue   
-        
-        i -=2
-    
+            # jumps if one correct index and A != 0
+            if len(jump_index) !=0:
+                # for jump_i in jump_index:
+                jump_i = jump_index[0]
+                queue.append((jump_i, new_output, new_reg))
+            else:
+                if i-2>=0:
+                    queue.append((i-2, new_output,new_reg))
+                    # print(queue)     
+ 
+        if len(output) ==0 and i ==0:
+            verif = assembly_instructions(new_reg.copy(), program.copy())
+            if verif == program:
+                print("index:",i, "out:",new_output, "A:", new_reg["A"],"verif:",verif)
+                return new_reg
+            
 
-    return registers["A"]
+    return None
 
 
 
@@ -177,7 +183,7 @@ registers, program = load_data(file_name)
 out_2 = assembly_auto_program(registers.copy(), program)
 # registers["A"] = out_2
 print("--------------------------")
-out = assembly_instructions(registers.copy(), program)
-
+out = assembly_instructions(registers.copy(), program, verbose=False)
+out_txt = ",".join(str(num) for num in out)
 print(out_2)
-print(out)
+print(out_txt)
