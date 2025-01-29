b40eaa930add4ef28e2ef52d9881a883bcb80ffe
Nicolas Samelson <salocin.sam@gmail.com>
2025-01-29 18:07:44 +0100

    all examples work but not input data

--- dec-12 retry.py
+++ dec-12 retry.py
@@ -21,60 +21,125 @@ def load_data(file_name):
 #   - cost = perim * area
 
 
-def compute_fencing_cost(data):
-    tot_cost = 0
-
-    # get different plant types
-    plant_types = set().union(*data)
+def get_clusters(data):
+    rows, cols = len(data), len(data[0])
 
     # Precompute neighbor offsets (4-directional adjacency)
     neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    visited = set()
 
-    all_clusters = []
+    def bfs(coord):
+        queue = deque([coord])
+        cluster = []
 
-    for plant_type in plant_types:
-        coords = [(i, j) for i in range(len(data)) for j in range(len(data[i])) if data[i][j] == plant_type]
-        
-        visited = set()
-
-        for coord in coords:
-            if coord in visited:
+        while queue:
+            x,y = queue.popleft()
+            plant = data[x][y]
+            if (x,y) in visited:
                 continue
-            
-            queue = deque([coord])           
-            new_cluster = []
 
-            while queue:
-                x,y = queue.popleft()
+            visited.add((x,y))
+            cluster.append((x,y))
 
-                if (x,y) in visited:
-                    continue
+            for dx,dy in neighbors:
+                nx,ny = (x+dx, y+dy)
+                if 0 <= nx < rows and 0 <= ny < cols:
+                    if data[nx][ny]==plant and (nx,ny) not in visited:
+                        queue.append((nx,ny))
+        return cluster
 
-                visited.add((x,y))
-                new_cluster.append((x,y))
+    # BIM!!!
+    return [bfs((i,j)) for i in range(rows) for j in range(cols) if (i,j) not in visited]
 
-                for dx,dy in neighbors:
-                    neighbor = (x+dx, y+dy)
-                    if neighbor in coords and neighbor not in visited:
-                        queue.append(neighbor)
+def compute_fencing_cost(data):
+    clusters = get_clusters(data)
+    neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    tot_cost = 0
 
-            all_clusters.append(new_cluster)
+    for cluster in clusters:
+        area = len(cluster)
+        perimeter = 0
 
+        for (x,y) in cluster:
+            perimeter += sum([1 for dx,dy in neighbors if (x+dx, y+dy) not in cluster])
 
-    print(len(all_clusters))
+        tot_cost += area * perimeter
 
     return tot_cost
+
+# Task 2:
+#   - count number of contiguous sides
+
+def compute_bulk_cost(data):
+    clusters = get_clusters(data)
+    row_neighbors = [(0, -1), (0, 1)]
+    col_neighbors = [(-1, 0), (1, 0)]
+    
+
+    def bfs(visited, cluster, coord, neighbors):
+        queue = [coord]
+        sub_cluster = []
+
+        while queue:
+            x,y = queue.pop(0)
+
+            if (x,y) in visited:
+                continue
+
+            visited.add((x,y))
+            sub_cluster.append((x,y))
+
+            for dx,dy in neighbors:
+                nx,ny = (x+dx, y+dy)
+                if (nx,ny) in cluster and (nx,ny) not in visited:
+                    queue.append((nx,ny))
+
+        return sub_cluster, visited
+
+
+    tot_cost = 0
+    for cluster in clusters:
+        area = len(cluster)
+        perimeter = 0
+
+        h_visited = set()
+        v_visited = set()
+
+        for coord in cluster:
+            sides = []
+
+            # check horizontal sub_clusters
+            if coord not in h_visited:
+                horiz_cluster, h_visited = bfs(h_visited,cluster,coord,row_neighbors)
+                # find free top and bottom sides
+                sides.extend([[(x+dx, y+dy) not in cluster for x,y in horiz_cluster ] for dx,dy in col_neighbors ])
+
+            # check vertical sub_clusters
+            if coord not in v_visited:
+                vert_cluster, v_visited = bfs(v_visited,cluster,coord,col_neighbors)
+                # find free left and right sides
+                sides.extend([[(x+dx, y+dy) not in cluster for  x,y in vert_cluster ] for dx,dy in row_neighbors])
+                
+            # Count the number of grouped True statements [True,True,False,True] = 2 groups = 2 free sides
+            perimeter += sum([len([list(j) for i,j in itertools.groupby(side) if i==True]) for side in sides])
+
+        tot_cost += area * perimeter
+        # print(cluster, area, perimeter, area * perimeter)
+    
+    return tot_cost
+
 
 
 # RUN
 
 
 file_name = "data/example.txt"
-file_name = "data/dec-12.txt"
+# file_name = "data/dec-12.txt"
 
 data = load_data(file_name)
 
-out = compute_fencing_cost(data)
+# out = compute_fencing_cost(data)
+out = compute_bulk_cost(data)
 
 
 print("tot cost: ", out)
