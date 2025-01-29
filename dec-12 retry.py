7c59e05014597bf7f3ed023585e5168a8745d999
Nicolas Samelson <salocin.sam@gmail.com>
2025-01-29 19:46:17 +0100

    it was that easy with the corners

--- dec-12 retry.py
+++ dec-12 retry.py
@@ -70,31 +70,10 @@ def compute_fencing_cost(data):
 # Task 2:
 #   - count number of contiguous sides
 
-def compute_bulk_cost(data):
+def compute_cost_from_corners(data):
+
     clusters = get_clusters(data)
-    row_neighbors = [(0, -1), (0, 1)]
-    col_neighbors = [(-1, 0), (1, 0)]
-    
-
-    def bfs(visited, cluster, start, neighbors):
-        queue = deque([start])
-        sub_cluster = []
-
-        while queue:
-            x, y = queue.popleft()
-
-            if (x,y) in visited:
-                continue
-
-            visited.add((x,y))
-            sub_cluster.append((x,y))
-
-            for dx,dy in neighbors:
-                nx,ny = (x+dx, y+dy)
-                if (nx,ny) in cluster and (nx,ny) not in visited:
-                    queue.append((nx,ny))
-
-        return sub_cluster
+    offsets = list(itertools.product([-1,1],repeat=2))
 
 
     tot_cost = 0
@@ -102,35 +81,87 @@ def compute_bulk_cost(data):
         area = len(cluster)
         perimeter = 0
 
-        h_visited = set()
-        v_visited = set()
+        for (x,y) in cluster:
 
-        for coord in cluster:
-            sides = []
-
-            # check horizontal sub_clusters
-            if coord not in h_visited:
-                horiz_cluster = bfs(h_visited,cluster,coord,row_neighbors)
-                h_visited.update(horiz_cluster)
-
-                # find free top and bottom sides
-                sides.extend([[(x+dx, y+dy) not in cluster for x,y in horiz_cluster ] for dx,dy in col_neighbors ])
-
-            # check vertical sub_clusters
-            if coord not in v_visited:
-                vert_cluster = bfs(v_visited,cluster,coord,col_neighbors)
-                v_visited.update(vert_cluster)
-
-                # find free left and right sides
-                sides.extend([[(x+dx, y+dy) not in cluster for  x,y in vert_cluster ] for dx,dy in row_neighbors])
+            for dx,dy in offsets:
+                corner = dx + x, dy+y    # corner
+                neigh_1 = dx + x, y     # vert neighbor
+                neigh_2 = x, dy + y     # horiz neighbor
                 
-            # Count the number of grouped True statements [True,True,False,True] = 2 groups = 2 free sides
-            perimeter += sum([len([list(j) for i,j in itertools.groupby(side) if i==True]) for side in sides])
+                # case 1: outside corner
+                if neigh_1 not in cluster and neigh_2 not in cluster:
+                    perimeter += 1
+                
+                # case 2: inside corner
+                if corner not in cluster and neigh_1 in cluster and neigh_2 in cluster:
+                    perimeter += 1
 
-        tot_cost += area * perimeter
         # print(area, "*", perimeter,"=", area * perimeter)
-    
+        tot_cost += area * perimeter
+
     return tot_cost
+
+# def compute_bulk_cost(data):
+#     clusters = get_clusters(data)
+#     row_neighbors = [(0, -1), (0, 1)]
+#     col_neighbors = [(-1, 0), (1, 0)]
+    
+
+#     def bfs(visited, cluster, start, neighbors):
+#         queue = deque([start])
+#         sub_cluster = []
+
+#         while queue:
+#             x, y = queue.popleft()
+
+#             if (x,y) in visited:
+#                 continue
+
+#             visited.add((x,y))
+#             sub_cluster.append((x,y))
+
+#             for dx,dy in neighbors:
+#                 nx,ny = (x+dx, y+dy)
+#                 if (nx,ny) in cluster and (nx,ny) not in visited:
+#                     queue.append((nx,ny))
+
+#         return sub_cluster
+
+
+#     tot_cost = 0
+#     for cluster in clusters:
+#         area = len(cluster)
+#         perimeter = 0
+
+#         h_visited = set()
+#         v_visited = set()
+
+#         for coord in cluster:
+#             sides = []
+
+#             # check horizontal sub_clusters
+#             if coord not in h_visited:
+#                 horiz_cluster = bfs(h_visited,cluster,coord,row_neighbors)
+#                 h_visited.update(horiz_cluster)
+
+#                 # find free top and bottom sides
+#                 sides.extend([[(x+dx, y+dy) not in cluster for x,y in horiz_cluster ] for dx,dy in col_neighbors ])
+
+#             # check vertical sub_clusters
+#             if coord not in v_visited:
+#                 vert_cluster = bfs(v_visited,cluster,coord,col_neighbors)
+#                 v_visited.update(vert_cluster)
+
+#                 # find free left and right sides
+#                 sides.extend([[(x+dx, y+dy) not in cluster for  x,y in vert_cluster ] for dx,dy in row_neighbors])
+                
+#             # Count the number of grouped True statements [True,True,False,True] = 2 groups = 2 free sides
+#             perimeter += sum([len([list(j) for i,j in itertools.groupby(side) if i==True]) for side in sides])
+
+#         tot_cost += area * perimeter
+#         print(area, "*", perimeter,"=", area * perimeter)
+    
+#     return tot_cost
 
 
 
@@ -143,7 +174,8 @@ file_name = "data/dec-12.txt"
 data = load_data(file_name)
 
 # out = compute_fencing_cost(data)
-out = compute_bulk_cost(data)
+# out = compute_bulk_cost(data)
+out = compute_cost_from_corners(data)
 
 
 print("tot cost: ", out)
